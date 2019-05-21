0ca07a9a9dd7329f409e4ab273233ac218ea4cd7
Anuraag Naidu Sanakkayala <anuraagsan7@gmail.com>
2019-05-21 18:25:33 -0500

    neww

---
+++ app/components/aboutme/module.js
@@ -0,0 +1,21 @@
+let myapp= angular.module("Myapp",[]);
+
+let mycontroller = function($scope){
+    $scope.message="Hello World";
+
+}
+
+myapp.controller("MyController", mycontroller)
+
+let JSONController = function($scope){
+    let Student = {
+        FirstName : "Raviteja",
+        LastName : "Saidurga",
+        Email: "anuraagsan729@gmail.com" 
+    }
+
+    $scope.Student = Student;
+
+}
+
+myapp.controller("Student", JSONController);
