d08ebd61e07f5828140f1cdfaf29b89246202d47
Anuraag Naidu Sanakkayala <anuraagsan7@gmail.com>
2019-05-22 19:11:16 -0500

    commit changes

--- app/components/aboutme/module.js
+++ app/components/aboutme/module.js
@@ -9,8 +9,8 @@ myapp.controller("MyController", mycontroller)
 
 let JSONController = function($scope){
     let Student = {
-        FirstName : "Raviteja",
-        LastName : "Saidurga",
+        FirstName : "Anurag",
+        LastName : "San",
         Email: "anuraagsan729@gmail.com" 
     }
 
